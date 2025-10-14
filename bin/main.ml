df0b3178f03779c28ba9922bbe3c00d28cc223e0
Emin Arslan <53535669+haxala1r@users.noreply.github.com>
2025-10-14 21:06:11 +0300

    Improved the repl to return to evaluation upon error. Also added an exit command

--- bin/main.ml
+++ bin/main.ml
@@ -8,12 +8,22 @@ let rec repl env c =
   let () = printf ">>> "; Out_channel.flush Out_channel.stdout; in
   match In_channel.input_line c with
   | None -> ()
-  | Some l -> 
-    let vals = (parse_str l) in
-    (* dbg_print_all vals; *)
-    dbg_print_all (eval_all env vals);
-    Out_channel.flush Out_channel.stdout;
-    repl env c;;
+  | Some "exit" -> ()
+  | Some l ->
+    try
+      let vals = (parse_str l) in
+      (* dbg_print_all vals; *)
+      dbg_print_all (eval_all env vals);
+      Out_channel.flush Out_channel.stdout;
+      repl env c
+    with
+    | Invalid_argument s ->
+      printf "%s\nResuming repl\n" s;
+      repl env c
+    | Parser.Error ->
+      printf "Expression '%s' couldn't be parsed, try again\n" l;
+      repl env c
+;;
 
 
-let _ = repl (make_env ()) (In_channel.stdin)
+let () = repl (make_env ()) (In_channel.stdin)
