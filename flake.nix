e10124498e8183476b0405015dbad6a40eb75e1a
haxala1r <me@emin.engineer>
2025-12-09 19:44:30 +0300

    Reorganize flake again, it should be good now

--- flake.nix
+++ flake.nix
@@ -7,18 +7,19 @@
   outputs = {self, nixpkgs}: 
   let
     pkgs = nixpkgs.legacyPackages.x86_64-linux;
-    nativeOcamlInputs = with pkgs.ocamlPackages; [menhir merlin dune_3];
-    nativeInputs = with pkgs; [ocaml];
+    devInputs = with pkgs.ocamlPackages; [merlin];
+    ocamlPkgs = with pkgs.ocamlPackages; [menhir dune_3];
+    nativeInputs =  with pkgs; ocamlPkgs ++ [ocaml]; 
   in
   {
     packages.x86_64-linux.default = pkgs.ocamlPackages.buildDunePackage {
       pname = "ollisp";
       version = "0.0.1";
       src = pkgs.lib.cleanSource ./.;
-      nativeBuildInputs = nativeInputs ++ nativeOcamlInputs;
+      nativeBuildInputs = nativeInputs;
     };
     devShells.x86_64-linux.default = pkgs.mkShell {
-      nativeBuildInputs = nativeInputs ++ nativeOcamlInputs;
+      nativeBuildInputs = nativeInputs ++ devInputs;
     };
   };
 }
