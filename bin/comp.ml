01bf01a9aecd5ca62d0d70c64c93970a72ac1e0c
Emin Arslan <me@emin.engineer>
2026-05-25 00:06:26 +0300

    added debug print functions to the Cps module to verify the transformation

--- bin/comp.ml
+++ bin/comp.ml
@@ -1,2 +1,8 @@
 
+let src = "
+           (f (g 5))"
+let c = Compiler.Core_ast.of_src src
+let _ = print_endline (match c with
+  | Ok (Compiler.Core_ast.Expr e :: _) -> Compiler.Cps.print_expr (Compiler.Cps.top_level e)
+  | _ -> "fuck")
 let _ = print_endline "hi"
--- lib/compiler/cps.ml
+++ lib/compiler/cps.ml
@@ -33,6 +33,30 @@ and expr =
   | If of value * expr * expr
   | Halt of value
 
+(* debug prints *)
+let p = Printf.sprintf
+let rec print_literal = function
+  | Core_ast.Nil -> p "()"
+  | (Int x) -> p "<int %d>" x
+  | (Double x) -> p "<double %f>" x
+  | (String s) -> p "<string \"%s\">" s
+  | (Symbol s) -> p "<symbol %s>" s
+  | (Cons (a, b)) -> p "(%s . %s)" (print_literal a) (print_literal b)
+
+let rec print_value = function
+  | Literal l -> print_literal l
+  | Var s -> p "%s" s
+  | Lambda (args, k, b) -> p "<lambda (%s) %s %s>" (List.fold_left (fun x y -> x ^ " " ^ y) "" args) k (print_expr b)
+  | Cont (k, e) -> p "<cont %s %s>" k (print_expr e)
+
+and print_expr = function
+  | LetVar (s, v, e) -> p "let %s = %s in\n%s" s (print_value v) (print_expr e)
+  | Set (s, v, e) -> p "set %s = %s in\n%s" s (print_value v) (print_expr e)
+  | App (f, args, k) -> p "(%s %s %s)" (print_value f) (List.fold_left (fun x y -> x ^ " " ^ (print_value y)) "" args) (print_value k)
+  | CApp (k, v) -> p "(%s %s)" (print_value k) (print_value v)
+  | If (t, th, el) -> p "if %s then %s else %s" (print_value t) (print_expr th) (print_expr el)
+  | Halt v -> print_value v
+
 let gensym = Gensym.gensym
 
 let rec cps (e : Core_ast.expression) (k : value -> expr) : expr =
@@ -71,3 +95,6 @@ let rec cps (e : Core_ast.expression) (k : value -> expr) : expr =
     | Begin (e :: rest) ->
        cps e (fun _ -> cps (Begin rest) k)
   )
+
+let top_level e =
+  cps e (fun v -> Halt v)
